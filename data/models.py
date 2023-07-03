7d880b5c52b9131baf8b0da49beedae21d9778d1
MADANI MOHAMED EL-HABIB <90145613+madanimohadhabib@users.noreply.github.com>
2023-07-03 21:34:09 +0200

    opgi

--- data/models.py
+++ data/models.py
@@ -1,80 +1,78 @@
 from django.db import models  
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-#from django.db import connections
+from django.db import connections
 #from channels.layers import  get_channel_layer
 #from asgiref.sync import async_to_sync
 import json
 class wilaya(models.Model):
 
-   lib_wilaya = models.CharField(max_length=120,db_column='lib_wilaya')
-   date_joined= models.DateTimeField(verbose_name='date joined', auto_now_add=True,db_column='date_joined') 
+   lib_wilaya = models.CharField(max_length=120)
+   date_joined= models.DateTimeField(verbose_name='date joined', auto_now_add=True) 
 
    def __str__(self):
             return self.lib_wilaya
 
-   
+  
         
 class Unite(models.Model):
-        lib_unit = models.CharField(max_length=120,db_column='lib_unit')
-        wilaya = models.ForeignKey(wilaya, on_delete=models.SET,db_column='wilaya_id')
-        date_joined= models.DateTimeField(verbose_name='date joined', auto_now_add=True,db_column='date_joined') 
+        lib_unit = models.CharField(max_length=120)
+        wilaya = models.ForeignKey(wilaya, on_delete=models.SET)
+        date_joined= models.DateTimeField(verbose_name='date joined', auto_now_add=True) 
 
         def __str__(self):
             return self.lib_unit
-       
+        
 
 class Cite(models.Model):
-              lib_Cite = models.CharField(max_length=120,db_column='lib_Cite')
-              unite = models.ForeignKey(Unite, on_delete=models.SET,db_column='unite_id')
-              nb_logts = models.PositiveIntegerField(db_column='nb_logts')
-              date_joined= models.DateTimeField(verbose_name='date joined', auto_now_add=True,db_column='date_joined') 
+              lib_Cite = models.CharField(max_length=120)
+              unite = models.ForeignKey(Unite, on_delete=models.SET)
+              nb_logts = models.PositiveIntegerField()
+              date_joined= models.DateTimeField(verbose_name='date joined', auto_now_add=True) 
 
               def __str__(self):
                  return self.lib_Cite
               
 
 class Batiment (models.Model):
-           lib_Batiment = models.CharField(max_length=120,db_column='lib_Batiment')
-           Cite = models.ForeignKey(Cite, on_delete=models.SET,db_column='Cite_id')
-           nb_logts = models.PositiveIntegerField(db_column='nb_logts')
-           nb_etage = models.PositiveIntegerField(db_column='nb_etage')
-           date_joined= models.DateTimeField(verbose_name='date joined', auto_now_add=True,db_column='date_joined') 
+           lib_Batiment = models.CharField(max_length=120)
+           Cite = models.ForeignKey(Cite, on_delete=models.SET)
+           nb_logts = models.PositiveIntegerField()
+           nb_etage = models.PositiveIntegerField()
+           date_joined= models.DateTimeField(verbose_name='date joined', auto_now_add=True) 
 
            def __str__(self):
                  return self.lib_Batiment
-           
           
-class Occupant (models.Model):
-       oc_id  = models.PositiveIntegerField(unique=True,db_column='oc_id')
-       nom_oc = models.CharField(max_length=120,db_column='nom_oc')
-       prenom_oc = models.CharField(max_length=120,db_column='prenom_oc')
-       date_naiss = models.DateTimeField(null=True,db_column='date_naiss')
-       lieu_naiss=models.CharField(max_length=120,db_column='lieu_naiss')
 
-       created_at = models.DateTimeField(auto_now_add=True,db_column='created_at')
+class Occupant (models.Model):
+       oc_id  = models.PositiveIntegerField(unique=True)
+       nom_oc = models.CharField(max_length=120)
+       prenom_oc = models.CharField(max_length=120)
+       date_naiss = models.DateTimeField(null=True)
+       lieu_naiss=models.CharField(max_length=120)
+
+       created_at = models.DateTimeField(auto_now_add=True)
 
        def __str__(self):
                  return self.nom_oc
        
 
-
 class Contrat(models.Model):
-    occupant = models.ForeignKey(Occupant, on_delete=models.SET,db_column='occupant_id')
-    date_cnt = models.DateTimeField(null=True,db_column='date_cnt')
-    date_strt_loyer = models.DateTimeField(null=True,db_column='date_strt_loyer')
-    loyer = models.FloatField(db_column='loyer')
-    charge = models.CharField(max_length=120,db_column='charge')
-    mnt_tva = models.FloatField(db_column='mnt_tva')
+    occupant = models.ForeignKey(Occupant, on_delete=models.SET)
+    date_cnt = models.DateTimeField(null=True)
+    date_strt_loyer = models.DateTimeField(null=True)
+    loyer = models.FloatField()
+    charge = models.CharField(max_length=120)
+    mnt_tva = models.FloatField()
     
    
     
-    total_of_month = models.FloatField(default=0,db_column='total_of_month')
+    total_of_month = models.FloatField(default=0)
   #siglaler add to changer total 
     def __str__(self):
                  return self.occupant.nom_oc
-    
-
+ 
 
  
 @receiver(post_save, sender=Contrat)
@@ -100,25 +98,26 @@ def update_total_of_month(sender, instance, **kwargs):
        sender.objects.filter(id=instance.id).update(total_of_month=total)
 
 class Logement (models.Model):
-        batiment = models.ForeignKey(Batiment, on_delete=models.SET,db_column='batiment_id')
-        contrat = models.ForeignKey(Contrat, on_delete=models.SET,db_column='contrat_id')
-        surface=  models.FloatField( default='m2',db_column='surface')
-        prix_logement=  models.FloatField(db_column='prix_logement')
-        type_logement=  models.CharField(max_length=120,db_column='type_logement')
+        batiment = models.ForeignKey(Batiment, on_delete=models.SET)
+        contrat = models.ForeignKey(Contrat, on_delete=models.SET)
+        surface=  models.FloatField( default='m2')
+        prix_logement=  models.FloatField()
+        type_logement=  models.CharField(max_length=120)
 
-        created_at = models.DateTimeField(auto_now_add=True,db_column='created_at')
+        created_at = models.DateTimeField(auto_now_add=True)
         def __str__(self):
                  return self.contrat.occupant.nom_oc
         
 
 class Consultation (models.Model):
-                logement = models.ForeignKey(Logement, on_delete=models.SET,db_column='logement_id')
-                occupant = models.ForeignKey(Occupant, on_delete=models.SET,db_column='occupant_id')
-                unite = models.ForeignKey(Unite, on_delete=models.SET,db_column='unite_id')
+                logement = models.ForeignKey(Logement, on_delete=models.SET)
+                occupant = models.ForeignKey(Occupant, on_delete=models.SET)
+                unite = models.ForeignKey(Unite, on_delete=models.SET)
 
-                mois=models.PositiveIntegerField(db_column='mois')
-                created_at = models.DateTimeField(auto_now_add=True,db_column='created_at')
-                total =models.FloatField(db_column='total')
+                mois=models.PositiveIntegerField()
+                created_at = models.DateTimeField(auto_now_add=True)
+
+                total =models.FloatField()
                 
                 def __str__(self):
                     return self.occupant.nom_oc
@@ -126,29 +125,7 @@ class Consultation (models.Model):
                
                 
 
-                def calculer_dette(self):
-                        contrat_values = Contrat.objects.values().get(id=self.occupant.id)
-                        diff = self.created_at - contrat_values['date_strt_loyer']
-                        mois_entiers = int(diff.total_seconds() / 2628000)
-                        montant_dette = contrat_values['total_of_month'] * (mois_entiers)
-                        paye = contrat_values['total_of_month'] * self.mois
-                        montant_dette = paye - montant_dette
-                        
-                        if mois_entiers <= 0  or self.mois > mois_entiers:
-                            mois_entiers = 0
-                            montant_dette = 0
-                        
-                        self.montant_dette = abs(montant_dette)
-
-                        if montant_dette == 0:
-                                self.status = 'En règle'
-                        elif montant_dette > contrat_values['total_of_month']:
-                                 self.status = 'En dette'
-                        else:
-                                self.status = 'En dette'
-                        
-
-                        return montant_dette
+        
      
 
 
