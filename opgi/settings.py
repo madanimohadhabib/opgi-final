ec05b9f53951cf9b73a772b1d1dfe257d8aaf092
MADANI MOHAMED EL-HABIB <90145613+madanimohadhabib@users.noreply.github.com>
2023-07-04 02:25:35 +0200

    opgi final

--- opgi/settings.py
+++ opgi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jua+cltqucqh(i_-b%4aeu_x+)-dd505_cnk-*^r@c-(r#xdk5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
